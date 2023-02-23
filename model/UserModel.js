51915da92b66e27a921fbd05314cca1fbf903ad7
rohit <rohitgadekar6@gmail.com>
2023-02-23 09:55:21 +0530

    api doc updated

--- model/UserModel.js
+++ model/UserModel.js
@@ -1,15 +1,4 @@
 const mongoose = require("mongoose");
-// const userschema = mongoose.Schema({
-//     name:String,
-//     email:String,
-//     password:String,
-//     phone:Number,
-//     roles:String
-// })
-
-// mongoose.model("users",userschema);
-// module.exports=mongoose.model("users")
-
 const userSchema  = new mongoose.Schema({
     name:{ type : String, required: true },
     email:{ type : String, required: true, unique : true },
